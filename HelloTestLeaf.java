d551c81da3b2470ac7f56d6c866f9e81f16bf7f2
kaviarasu92 <kavi.arasu101@gmail.com>
2025-05-24 15:43:37 +0530

    Add files via upload

---
+++ HelloTestLeaf.java
@@ -0,0 +1,17 @@
+package week1.day1;
+//Comment Line 
+
+//AccessModifer Keyword Classname
+//Blueprint to write the code
+//Blueprint to create an object 
+
+public class HelloTestLeaf {
+	//Declaring main method
+	public static void main(String args[])
+	{
+		//Declaring Sysout statment
+		
+		System.out.println("Welcome To TestLeaf");
+	}
+
+}
